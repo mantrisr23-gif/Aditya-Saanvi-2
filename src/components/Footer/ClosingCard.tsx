82b2f548dbba7c353814f367e5e4f9dd8af60909
mantrisr23-gif <mantrisr23@gmail.com>
2026-06-07 06:15:12 +0530

    Update ClosingCard.tsx

--- src/components/Footer/ClosingCard.tsx
+++ src/components/Footer/ClosingCard.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { WeddingData } from "../../types/wedding";
-
 interface ClosingProps {
   data: WeddingData;
 }
