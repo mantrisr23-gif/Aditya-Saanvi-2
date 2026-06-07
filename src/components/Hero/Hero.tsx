592213641979b7b24c4e5b5a25026fc7c85c4031
mantrisr23-gif <mantrisr23@gmail.com>
2026-06-07 06:15:35 +0530

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { WeddingData } from "../../data/weddingData";
+import { WeddingData } from "../../types/wedding";
 
 interface HeroProps {
   data: WeddingData;
